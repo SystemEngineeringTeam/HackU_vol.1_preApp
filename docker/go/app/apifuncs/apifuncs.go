d30fca34d306d53cf745af37448e579630f1f073
happy663 <tatu2425@gmail.com>
2020-08-11 16:55:14 +0900

    error

--- docker/go/app/apifuncs/apifuncs.go
+++ docker/go/app/apifuncs/apifuncs.go
@@ -1,9 +1,51 @@
 package apifuncs
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"fmt"
 	"net/http"
 )
 
-func test(w http.ResponseWriter, r *http.Request) {
 
+type InputJsonSchema struct{
+
+	In string `json:"in"`
+	Name string `json:"name"`
+	Description string `json:"desciption"`
+	Required bool `json:"required"`
+	Responses string `schema:"responses"`
+}
+
+func Test(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == http.MethodGet {
+		fmt.Fprintln(w, "Get Method")
+	
+	  } else if r.Method== http.MethodPost{
+		fmt.Fprintln(w, "Post Method")
+
+
+		b, err:=ioutil.ReadAll(r.Body)
+		if err !=nil{
+			fmt.Println("io error")
+			return 
+		}
+
+		jsonBytes := ([]byte)(b)
+		data:=json.Unmarshal(jsonBytes,data);err!=nil{
+			fmt.Println("JSON Unmarashal erro",err)
+			return 					
+		}
+		fmt.Fprintf(w,data.In)
+		fmt.Fprintf(w,data.Name)
+		fmt.Fprintf(w,data.Description)
+		fmt.Fprintf(w,data.Required)
+		fmt.Fprintf(w,data.Responses)
+										
+	  }else if r.Method==http.MethodPut{
+		fmt.Fprintln(w,"Put Method")  			
+	  }else{
+		fmt.Fprintln(w,"delete Method")			
+	  }
 }
--- docker/go/app/main.go
+++ docker/go/app/main.go
@@ -2,38 +2,16 @@ package main
 
 import(
 	"fmt"
-	"net/http"
-	"io/ioutil"
-	"encoding/json"
+	"net/http"	
+	"set1.ie.aitech.ac.jp/HackU_vol_1/apifuncs"
 )
-
-type InputJsonSchema struct{
-
-	Name string `json:"name"`
-}
 
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
-		if r.Method == http.MethodGet {
-			fmt.Fprintln(w, "Get Method")
+		apifuncs.Test(w,r)
 		
-		  } else if r.Method== http.MethodPost{
-			fmt.Fprintln(w, "Post Method")
-
-			fmt.Fprintln(w,r.FormValue("in"))
-			fmt.Fprintln(w,r.FormValue("name"))
-			fmt.Fprintln(w,r.FormValue("required"))
-			fmt.Fprintln(w,r.FormValue("schema"))
-			
-	
-		  }else if r.Method==http.MethodPut{
-			fmt.Fprintln(w,"Put Method")  			
-		  }else{
-			fmt.Fprintln(w,"delete Method")			
-		  }
-						  
 	  fmt.Fprintln(w, "Hello World")	  	  
 	})
 

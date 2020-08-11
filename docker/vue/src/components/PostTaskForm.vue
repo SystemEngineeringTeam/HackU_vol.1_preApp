19674b1384276d604e197c024b08cc617adb21b6
kenshin1025 <sukoyakamind1025@gmail.com>
2020-08-11 19:39:27 +0900

    axios使えるようになった

---
+++ docker/vue/src/components/PostTaskForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card>
+    <v-card-text>
+      <v-text-field v-model="title" label="title" required></v-text-field>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn color="primary" @click="test">
+        作成
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    title: "こんにちは",
+  }),
+  methods:{
+    test(){
+      console.log(this.title);
+      this.axios
+      .get("https://api.coindesk.com/v1/bpi/currentprice.json")
+      .then((response) => console.log(response));
+    }
+  }
+};
+</script>
--- docker/vue/src/pages/ToDo.vue
+++ docker/vue/src/pages/ToDo.vue
@@ -1,6 +1,9 @@
 <template>
   <v-container>
     <v-row class="text-center">
+      <v-col cols="12">
+        <PostTaskForm />
+      </v-col>
       <v-col cols="12" v-for="(task, i) in sample" :key="i">
         <Task :task="task" :users="findUser(task.users_id)" />
       </v-col>
@@ -10,13 +13,44 @@
 
 <script>
 import Task from "../components/Task";
+import PostTaskForm from "../components/PostTaskForm";
 
 export default {
   name: "ToDo",
 
   components: {
     Task,
+    PostTaskForm,
   },
+
+  data: () => ({
+    sample: [
+      {
+        id: 0,
+        title: "醤油を買う",
+        deadline: "2020-08-24 09:00",
+        users: [0, 1],
+      },
+      {
+        id: 1,
+        title: "ペットの散歩",
+        deadline: "2020-08-24 15:00",
+        users: [2, 3, 1],
+      },
+      {
+        id: 2,
+        title: "スカイダイビング",
+        deadline: "2020-08-11 15:00",
+        users: [1, 2],
+      },
+    ],
+    users: [
+      { id: 0, name: "けんしん" },
+      { id: 1, name: "福田" },
+      { id: 2, name: "あいはた" },
+      { id: 3, name: "とやま" },
+    ],
+  }),
 
   methods: {
     findUser: function(taskUser) {
@@ -27,34 +61,10 @@ export default {
       return re;
     },
   },
-
-  data: () => ({
-    sample: [
-      {
-        id: 0,
-        title: "醤油を買う",
-        deadline: "2020-08-24 09:00",
-        users_id: [0, 1],
-      },
-      {
-        id: 1,
-        title: "ペットの散歩",
-        deadline: "2020-08-24 15:00",
-        users_id: [2, 3, 1],
-      },
-      {
-        id: 2,
-        title: "スカイダイビング",
-        deadline: "2020-08-11 15:00",
-        users_id: [1, 2],
-      },
-    ],
-    users: [
-      { id: 0, name: "けんしん" },
-      { id: 1, name: "福田" },
-      { id: 2, name: "あいはた" },
-      { id: 3, name: "とやま" },
-    ],
-  }),
+  // created() {
+  //   this.axios
+  //     .get("https://api.coindesk.com/v1/bpi/currentprice.json")
+  //     .then((response) => console.log(response));
+  // },
 };
 </script>

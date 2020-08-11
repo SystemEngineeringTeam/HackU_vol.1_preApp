fca77831668d896822cb296c17e45ecd5753661a
happy663 <tatu2425@gmail.com>
2020-08-11 15:26:48 +0900

    分岐させた

--- docker/go/app/main.go
+++ docker/go/app/main.go
@@ -3,14 +3,40 @@ package main
 import(
 	"fmt"
 	"net/http"
+	"io/ioutil"
+	"encoding/json"
 )
+
+type InputJsonSchema struct{
+
+	Name string `json:"name"`
+}
 
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	  fmt.Fprintln(w, "Hello World")
-	})
+
+		if r.Method == http.MethodGet {
+			fmt.Fprintln(w, "Get Method")
+		
+		  } else if r.Method== http.MethodPost{
+			fmt.Fprintln(w, "Post Method")
+
+			fmt.Fprintln(w,r.FormValue("in"))
+			fmt.Fprintln(w,r.FormValue("name"))
+			fmt.Fprintln(w,r.FormValue("required"))
+			fmt.Fprintln(w,r.FormValue("schema"))
+			
 	
+		  }else if r.Method==http.MethodPut{
+			fmt.Fprintln(w,"Put Method")  			
+		  }else{
+			fmt.Fprintln(w,"delete Method")			
+		  }
+						  
+	  fmt.Fprintln(w, "Hello World")	  	  
+	})
+
 	http.ListenAndServe(":8080", nil)	
   }
 

62fbc2cb65ae384be61f08b04ccff3915b8ab9eb
kenshin1025 <sukoyakamind1025@gmail.com>
2020-08-12 13:38:07 +0900

    datepickerをstoreを使うようにした

--- docker/vue/src/store/index.js
+++ docker/vue/src/store/index.js
@@ -1,11 +1,31 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from 'axios';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    tasks: [],
+    tasks: [
+      {
+        id: 0,
+        title: "醤油をかける",
+        deadline: "2020-08-24 09:00",
+        users_id: [0, 1],
+      },
+      {
+        id: 1,
+        title: "ペットの散歩",
+        deadline: "2020-08-24 15:00",
+        users_id: [2, 3, 1],
+      },
+      {
+        id: 2,
+        title: "スカイダイビング",
+        deadline: "2020-08-11 15:00",
+        users_id: [1, 2],
+      },
+    ],
     users: [],
     post: {
       title: "",
@@ -33,7 +53,7 @@ export default new Vuex.Store({
     pushPostUsers(state, user) {
       state.post.users.push(user);
     },
-    splicePostUsers(state, index) {
+    splicePostUser(state, index) {
       state.post.users.slice(index, 1);
     },
   },

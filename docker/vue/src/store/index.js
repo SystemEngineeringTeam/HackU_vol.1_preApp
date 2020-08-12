a658ccf4b1a786524fb5e37d70f5db4ffb247768
kenshin1025 <sukoyakamind1025@gmail.com>
2020-08-12 12:10:08 +0900

    ストアをmutationまで書いた

--- docker/vue/src/store/index.js
+++ docker/vue/src/store/index.js
@@ -4,8 +4,50 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    tasks: [],
+    users: [],
+    post: {
+      title: "",
+      deadlineDate: "",
+      deadlineTime: "",
+      users: [],
+    },
+  },
+  mutations: {
+    setTasks(state, tasks) {
+      state.tasks = tasks;
+    },
+    setUsers(state, users) {
+      state.users = users;
+    },
+    setPostTitle(state, title) {
+      state.post.title = title;
+    },
+    setPostDeadlineDate(state, date) {
+      state.post.deadlineDate = date;
+    },
+    setPostDeadlineTime(state, time) {
+      state.post.deadlineTime = time;
+    },
+    pushPostUsers(state, user) {
+      state.post.users.push(user);
+    },
+    splicePostUsers(state, index) {
+      state.post.users.slice(index, 1);
+    },
+  },
+  actions: {
+    async setTasks(context) {
+      await axios
+        .get("http://localhost:3000")
+        .then((res) => context.setTasks(res.data));
+    },
+    async setUsers(context) {
+      await axios
+        .get("http://localhost:3000")
+        .then((res) => context.setUsers(res.data));
+    },
+  },
+  modules: {},
 });

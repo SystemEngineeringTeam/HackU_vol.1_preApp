d0a6d7e439393b649382b3f08a7294ed12a8502b
tikuwa <haru11502@ezweb.ne.jp>
2020-08-12 10:09:21 +0900

    CORSに対応させた

--- docker/go/app/apifuncs/dontTouchThisFile.go
+++ docker/go/app/apifuncs/dontTouchThisFile.go
@@ -9,19 +9,29 @@ import (
 	"set1.ie.aitech.ac.jp/HackU_vol_1/dbctl"
 )
 
-// DBTest はデータベース関数のテストする関数です
+// DBTest is func that tests database funcs.
 func DBTest(w http.ResponseWriter, r *http.Request) {
+	// CORS settings.
+	w.Header().Set("Access-Control-Allow-Origin", r.RemoteAddr)              // Allow remote access.
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE") // Allowed methods.
+
 	if r.Method == http.MethodGet {
+		// CallTasks returns []dbctl.Task.
 		tasks, err := dbctl.CallTasks()
 		if err != nil {
 			log.Fatal(err)
+			w.WriteHeader(http.StatusServiceUnavailable)
 		}
+		// Array to JSON.
 		b, err := json.Marshal(tasks)
 		if err != nil {
 			log.Fatal(err)
 		}
+		// []byte to string.
 		taskString := string(b)
+
+		w.WriteHeader(200)
 		fmt.Fprintln(w, taskString)
-		fmt.Println(taskString, "hogehoge")
+		fmt.Println(taskString)
 	}
 }
